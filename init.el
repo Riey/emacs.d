d938122141735f0c5478c395dd436a213e625a53
Riey <creeper844@gmail.com>
2019-08-08 14:22:18 +0900

    Fix emacs package bug

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(setq package-check-signature nil)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
